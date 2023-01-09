a0e7e03f49935c01376b0b1d515914920b975306
Joe Gillotti <joe@u13.net>
2023-01-08 21:29:05 -0800

    Refactor to separate game logic + UI logic

---
+++ game.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"errors"
+	"github.com/gammazero/deque"
+	"math/rand"
+	"time"
+)
+
+type Type int
+
+const (
+	SNAKE Type = 1
+	FOOD  Type = 2
+)
+
+type Direction int
+
+const (
+	RIGHT = 1
+	LEFT  = 2
+	UP    = 3
+	DOWN  = 4
+)
+
+type Cell struct {
+	Type Type
+	Age  int
+}
+
+type Cord struct {
+	x, y int
+}
+
+type Snake struct {
+	board     map[Cord]Cell
+	direction Direction
+	tailQueue deque.Deque[Cord]
+}
+
+func (s *Snake) createFood() {
+	for i := 0; i < 100; i++ {
+		rand.Seed(time.Now().UnixNano())
+		x := rand.Intn(WIDTH - 1)
+		rand.Seed(time.Now().UnixNano())
+		y := rand.Intn(HEIGHT - 1)
+		cord := Cord{x: x, y: y}
+		if _, ok := s.board[cord]; !ok {
+			s.board[cord] = Cell{Type: FOOD}
+			break
+		}
+	}
+}
+
+func (s *Snake) createSnake(cord Cord) {
+	s.tailQueue.PushFront(cord)
+	s.board[cord] = Cell{Type: SNAKE}
+}
+
+func (s *Snake) Reset() {
+	// Wipe queue
+	s.tailQueue.Clear()
+
+	// Wipe Board
+	s.board = map[Cord]Cell{}
+
+	// Set default direction
+	s.direction = RIGHT
+
+	// Create a small 3-length snake pointing right
+	// in the middle
+	s.createSnake(Cord{x: WIDTH/2 - 1, y: HEIGHT / 2})
+	s.createSnake(Cord{x: WIDTH / 2, y: HEIGHT / 2})
+	s.createSnake(Cord{x: WIDTH/2 + 1, y: HEIGHT / 2})
+
+	// Create a food
+	s.createFood()
+}
+
+func (s *Snake) UpdateState() error {
+
+	// Add new to front.
+	next := s.tailQueue.At(0)
+	previous := s.tailQueue.At(1)
+
+	switch s.direction {
+	case LEFT:
+		next.x--
+	case RIGHT:
+		next.x++
+	case DOWN:
+		next.y++
+	case UP:
+		next.y--
+	}
+
+	// Can't change direction to trail. Sanity-check; this should be disallowed
+	if next.x == previous.x && next.y == previous.y {
+		return errors.New("can't go backwards")
+	}
+
+	// Boundary checks
+	if next.x < 0 || next.y < 0 || next.x >= WIDTH-1 || next.y >= HEIGHT-1 {
+		return errors.New("hit wall")
+	}
+
+	if cell, ok := s.board[next]; ok {
+		switch cell.Type {
+		case SNAKE:
+			// Eat self checks
+			return errors.New("ate self")
+		case FOOD:
+			// A food item? Don't pop last elem (get longer) and spawn new food
+			s.createFood()
+		}
+
+	} else {
+		// Kill final position (don't get longer)
+		oldest := s.tailQueue.PopBack()
+		delete(s.board, oldest)
+	}
+
+	// Good. Plant new one on board.
+	s.createSnake(next)
+
+	return nil
+}
+
+func (s *Snake) ChangeDirection(chosen Direction) {
+	next := s.tailQueue.At(0)
+	previous := s.tailQueue.At(1)
+
+	switch chosen {
+	case LEFT:
+		next.x--
+	case RIGHT:
+		next.x++
+	case DOWN:
+		next.y++
+	case UP:
+		next.y--
+	}
+
+	// Can't change direction to trail
+	if next.x == previous.x && next.y == previous.y {
+		// Ignore
+		return
+	}
+
+	s.direction = chosen
+}
--- main.go
+++ main.go
@@ -1,198 +1,54 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
-	"math/rand"
 	"time"
 
-	"github.com/gammazero/deque"
 	"github.com/gdamore/tcell"
 )
 
-type Type int
-
-const (
-	SNAKE Type = 1
-	FOOD  Type = 2
-)
-
-type Direction int
-
-const (
-	RIGHT = 1
-	LEFT  = 2
-	UP    = 3
-	DOWN  = 4
-)
+type UI struct {
+	screen tcell.Screen
+	snake  *Snake
+	paused bool
+	quit   chan struct{}
+}
 
 const (
 	WIDTH  = 30
 	HEIGHT = 30
 )
 
-type Cell struct {
-	Type Type
-	Age  int
-}
-
-type Cord struct {
-	x, y int
-}
-
-var board = map[Cord]Cell{}
-
-var (
-	direction Direction
-	tailQueue deque.Deque[Cord]
-)
-
-func initState() {
-	// Wipe Board
-	board = map[Cord]Cell{}
-
-	// Set default direction
-	direction = RIGHT
-
-	// Create a small 3-length snake pointing right
-	// in the middle
-	createSnake(Cord{x: WIDTH/2 - 1, y: HEIGHT / 2})
-	createSnake(Cord{x: WIDTH / 2, y: HEIGHT / 2})
-	createSnake(Cord{x: WIDTH/2 + 1, y: HEIGHT / 2})
-
-	// Create a food
-	createFood()
-}
-
-func createSnake(cord Cord) {
-	tailQueue.PushFront(cord)
-	board[cord] = Cell{Type: SNAKE}
-}
-
-func createFood() {
-	for i := 0; i < 100; i++ {
-		rand.Seed(time.Now().UnixNano())
-		x := rand.Intn(WIDTH - 1)
-		rand.Seed(time.Now().UnixNano())
-		y := rand.Intn(HEIGHT - 1)
-		cord := Cord{x: x, y: y}
-		if _, ok := board[cord]; !ok {
-			board[cord] = Cell{Type: FOOD}
-			break
-		}
-	}
-}
-
-// Try to move TIP one cell to the right
-// Change the oldest CELL tail to empty
-func frame() error {
-
-	// Add new to front.
-	next := tailQueue.At(0)
-	previous := tailQueue.At(1)
-
-	switch direction {
-	case LEFT:
-		next.x--
-	case RIGHT:
-		next.x++
-	case DOWN:
-		next.y++
-	case UP:
-		next.y--
-	}
-
-	// Can't change direction to trail. Sanity-check; this should be disallowed
-	if next.x == previous.x && next.y == previous.y {
-		return errors.New("can't go backwards")
-	}
-
-	// Boundary checks
-	if next.x < 0 || next.y < 0 || next.x >= WIDTH-1 || next.y >= HEIGHT-1 {
-		return errors.New("hit wall")
-	}
-
-	if cell, ok := board[next]; ok {
-		switch cell.Type {
-		case SNAKE:
-			// Eat self checks
-			return errors.New("ate self")
-		case FOOD:
-			// A food item? Don't pop last elem (get longer) and spawn new food
-			createFood()
-		}
-
-	} else {
-		// Kill final position (don't get longer)
-		oldest := tailQueue.PopBack()
-		delete(board, oldest)
-	}
-
-	// Good. Plant new one on board.
-	createSnake(next)
-
-	return nil
-}
-
-func changeDirection(chosen Direction) {
-	next := tailQueue.At(0)
-	previous := tailQueue.At(1)
-
-	switch chosen {
-	case LEFT:
-		next.x--
-	case RIGHT:
-		next.x++
-	case DOWN:
-		next.y++
-	case UP:
-		next.y--
-	}
-
-	// Can't change direction to trail
-	if next.x == previous.x && next.y == previous.y {
-		// Ignore
-		return
-	}
-
-	direction = chosen
-}
-
-func drawBoard() {
+func (g *UI) drawBoard() {
 	wall := tcell.StyleDefault.Foreground(tcell.ColorGrey).Background(tcell.ColorGrey)
 	snake := tcell.StyleDefault.Foreground(tcell.ColorBlue).Background(tcell.ColorBlue)
 	food := tcell.StyleDefault.Background(tcell.ColorRed).Foreground(tcell.ColorRed)
 
 	for i := 0; i < WIDTH+1; i++ {
-		screen.SetContent(i, 0, tcell.RuneHLine, nil, wall)
-		screen.SetContent(i, HEIGHT+1, tcell.RuneHLine, nil, wall)
+		g.screen.SetContent(i, 0, tcell.RuneHLine, nil, wall)
+		g.screen.SetContent(i, HEIGHT+1, tcell.RuneHLine, nil, wall)
 	}
 
 	for i := 0; i < HEIGHT+1; i++ {
-		screen.SetContent(0, i, tcell.RuneVLine, nil, wall)
-		screen.SetContent(WIDTH+1, i, tcell.RuneVLine, nil, wall)
+		g.screen.SetContent(0, i, tcell.RuneVLine, nil, wall)
+		g.screen.SetContent(WIDTH+1, i, tcell.RuneVLine, nil, wall)
 	}
 
-	for cord, cell := range board {
+	for cord, cell := range g.snake.board {
 		switch cell.Type {
 		case SNAKE:
-			screen.SetContent(cord.x+1, cord.y+1, tcell.RuneBlock, nil, snake)
+			g.screen.SetContent(cord.x+1, cord.y+1, tcell.RuneBlock, nil, snake)
 		case FOOD:
-			screen.SetContent(cord.x+1, cord.y+1, tcell.RuneDiamond, nil, food)
+			g.screen.SetContent(cord.x+1, cord.y+1, tcell.RuneDiamond, nil, food)
 		}
 	}
 }
 
-var screen tcell.Screen
-
-var quit = make(chan struct{})
-
 // TODO: add score/length counters underneath walls
 func main() {
-	var err error
 
-	screen, err = tcell.NewTerminfoScreen()
+	screen, err := tcell.NewTerminfoScreen()
 	if err != nil {
 		log.Fatalf("Failed making screen: %v", err)
 	}
@@ -205,21 +61,29 @@ func main() {
 		Background(tcell.ColorBlack).
 		Foreground(tcell.ColorWhite))
 
+	ui := &UI{
+		screen: screen,
+		snake:  &Snake{},
+	}
+	ui.Run()
+}
+
+func (g *UI) Run() {
 	evenChan := make(chan tcell.Event)
 
 	go func() {
 		for {
 			select {
-			case <-quit:
+			case <-g.quit:
 				return
 			default:
 			}
-			ev := screen.PollEvent()
+			ev := g.screen.PollEvent()
 			if ev == nil {
 				return
 			}
 			select {
-			case <-quit:
+			case <-g.quit:
 				return
 			case evenChan <- ev:
 			case <-time.After(time.Second):
@@ -232,28 +96,30 @@ func main() {
 	ticker := time.NewTicker(100 * time.Millisecond)
 
 	var endErr error
-	initState()
+	g.snake.Reset()
 
 LOOP:
 	for {
 		select {
-		case <-quit:
+		case <-g.quit:
 			break LOOP
 		case ev := <-evenChan:
-			handleEvent(ev)
+			g.handleEvent(ev)
 		case <-ticker.C:
-			endErr = frame()
-			if endErr != nil {
-				break LOOP
+			if !g.paused {
+				endErr = g.snake.UpdateState()
+				if endErr != nil {
+					break LOOP
+				}
 			}
-			screen.Clear()
-			drawBoard()
-			screen.Show()
+			g.screen.Clear()
+			g.drawBoard()
+			g.screen.Show()
 		}
 	}
 
-	screen.Clear()
-	screen.Fini()
+	g.screen.Clear()
+	g.screen.Fini()
 
 	if endErr != nil {
 		// TODO: replace this with a modal/popup with game over
@@ -262,28 +128,32 @@ LOOP:
 	} else {
 		fmt.Println("Exiting")
 	}
+
 }
 
-func handleEvent(ev tcell.Event) {
+func (g *UI) handleEvent(ev tcell.Event) {
 
 	switch ev := ev.(type) {
 	case *tcell.EventInterrupt:
-		close(quit)
+		close(g.quit)
 	case *tcell.EventKey:
 		switch ev.Key() {
 		case tcell.KeyEscape, tcell.KeyCtrlC:
-			close(quit)
+			close(g.quit)
 		case tcell.KeyUp:
-			changeDirection(UP)
+			g.snake.ChangeDirection(UP)
 		case tcell.KeyDown:
-			changeDirection(DOWN)
+			g.snake.ChangeDirection(DOWN)
 		case tcell.KeyLeft:
-			changeDirection(LEFT)
+			g.snake.ChangeDirection(LEFT)
 		case tcell.KeyRight:
-			changeDirection(RIGHT)
+			g.snake.ChangeDirection(RIGHT)
 		case tcell.KeyRune:
-			if ev.Rune() == 'q' {
-				close(quit)
+			switch ev.Rune() {
+			case 'q':
+				close(g.quit)
+			case 'p':
+				g.paused = !g.paused
 			}
 		}
 	}
